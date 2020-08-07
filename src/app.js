e413dc37ee9c4cd3e52dbbb82b31c309854d4a07
firos-coder <firosthathoor@gmail.com>
2020-08-07 18:33:23 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const path = require('path')
 const countryDetails = require('./utils/country')
 
 const app = express()
-const port = process.env | 3000
+const port = process.env.PORT || 3000
 
 const publicDirectory = path.join(__dirname, '../public/')
 const viewsPath = path.join(__dirname, '../templates/views/')
